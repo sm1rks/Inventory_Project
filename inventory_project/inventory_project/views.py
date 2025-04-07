6bd7d7ab1fa72ced1e25dce103355728e9388bd5
John Collins <jo873558@ucf.edu>
2025-04-07 18:49:13 -0400

    Initial commit

---
+++ inventory_project/inventory_project/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello World! I'm Home.")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("My About page.")
+    return render(request, 'about.html')

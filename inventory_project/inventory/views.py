02f8ef01c5409719962f588236f29596f31ea932
EthanE <jackestrad4@gmail.com>
2025-04-09 21:25:03 -0400

    trying to get the inventory to show for the slected store

--- inventory_project/inventory/views.py
+++ inventory_project/inventory/views.py
@@ -1,20 +1,31 @@
-from django.contrib.auth.decorators import login_required
+from inventory_project.decorators import google_login_required
 from django.shortcuts import render
-from .models import Store,Inventory
+from .models import Store, Inventory
 
-@login_required(login_url='/users/login/')
+
+@google_login_required(login_url='/users/')
 def inventory(request):
     store = None
     inventory_items = []
 
-    stores = request.GET.get('stores', None)
-    
-    if stores:
+    # Get the store ID from the GET request
+    store_id = request.GET.get('stores', None)
+
+    # Get all stores to populate the drop-down
+    stores = Store.objects.all()
+
+    if store_id:
         try:
-            Store.objects.get(id=stores)
-            inventory_items = Inventory.objects.filter(store=store)
+            # Get the selected store using the provided store ID
+            store = Store.objects.get(id=store_id)
             
+            # Get inventory items related to the selected store
+            inventory_items = Inventory.objects.filter(store=store)
         except Store.DoesNotExist:
             store = None
 
-    return render(request, 'inventory/store.html', {'stores': stores, 'selected_store': store, 'inventory_items': inventory_items})
+    return render(request, 'inventory/store.html', {
+        'stores': stores,
+        'selected_store': store,
+        'inventory_items': inventory_items
+    })

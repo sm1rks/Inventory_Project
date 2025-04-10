0578ea041eb0bcf4844da8e2b3a394a8e8594c42
EthanE <jackestrad4@gmail.com>
2025-04-09 20:29:02 -0400

    got the page working needs to be tweaked

--- inventory_project/inventory/views.py
+++ inventory_project/inventory/views.py
@@ -4,15 +4,16 @@ from .models import Store,Inventory
 
 @login_required(login_url='/users/login/')
 def inventory(request):
-    store = Store.objects.all()
-
-    stores = request.GET.get('stores', None)
+    store = None
     inventory_items = []
 
+    stores = request.GET.get('stores', None)
+    
     if stores:
         try:
             Store.objects.get(id=stores)
             inventory_items = Inventory.objects.filter(store=store)
+            
         except Store.DoesNotExist:
             store = None
 
